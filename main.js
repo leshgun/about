cf8e983aa0e1fbe97e5f63b252fd21c835ba367e
sergei_panin <s.panin1976@mail.ru>
2022-04-08 16:39:24 +0200

    added some functionality

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-let ham = document.querySelector(".aside-toggle");
-ham.addEventListener("click", function(){
+const aside_toggle = document.querySelector(".aside-toggle");
+const tile = document.querySelectorAll(".tile");
+
+aside_toggle.addEventListener("click", function(){
     document.querySelector("aside").classList.toggle("active");
-    document.querySelector("main").classList.toggle("active");
+    document.getElementById("wrapper").classList.toggle("active");
+    document.getElementById("aside-toggle-icon").classList.toggle("fa-arrow-right");
+    aside_toggle.classList.toggle("active");
     // console.log("Lohi")
 });
+
+console.log(tile);
+for (let i=0; i<tile.length; i++) {
+    const tile_name = tile[i].querySelector(".tile-name");
+    tile_name.addEventListener("click", () => {
+        // tile[i].querySelector(".tile-content").classList.toggle("active");
+        tile[i].classList.toggle("active");
+    })
+    // console.log(tile[i].querySelector(".tile-name"));
+}
