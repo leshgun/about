9a55816c7fdaa6ef7bcb9453712e29c53dd63965
sergei_panin <s.panin1976@mail.ru>
2022-04-29 17:22:58 +0200

    add main things

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
-const aside_toggle = document.querySelector(".aside-toggle");
-const tile = document.querySelectorAll(".tile");
+// const aside_toggle = document.querySelector(".aside-toggle");
+// const tile = document.querySelectorAll(".tile");
 
-aside_toggle.addEventListener("click", function(){
-    document.querySelector("aside").classList.toggle("active");
-    document.getElementById("wrapper").classList.toggle("active");
-    document.getElementById("aside-toggle-icon").classList.toggle("fa-arrow-right");
-    aside_toggle.classList.toggle("active");
-    // console.log("Lohi")
-});
+// aside_toggle.addEventListener("click", function(){
+//     document.querySelector("aside").classList.toggle("active");
+//     document.getElementById("wrapper").classList.toggle("active");
+//     document.getElementById("aside-toggle-icon").classList.toggle("fa-arrow-right");
+//     aside_toggle.classList.toggle("active");
+//     // console.log("Lohi")
+// });
 
-console.log(tile);
-for (let i=0; i<tile.length; i++) {
-    const tile_name = tile[i].querySelector(".tile-name");
-    tile_name.addEventListener("click", () => {
-        // tile[i].querySelector(".tile-content").classList.toggle("active");
-        tile[i].classList.toggle("active");
-    })
-    // console.log(tile[i].querySelector(".tile-name"));
+// console.log(tile);
+// for (let i=0; i<tile.length; i++) {
+//     const tile_name = tile[i].querySelector(".tile-name");
+//     tile_name.addEventListener("click", () => {
+//         // tile[i].querySelector(".tile-content").classList.toggle("active");
+//         tile[i].classList.toggle("active");
+//     })
+//     // console.log(tile[i].querySelector(".tile-name"));
+// }
+
+const clipboard = document.querySelector(".clipboard");
+const clipboard_content = document.querySelector(".clipboard-content");
+const cork_board = document.getElementById("cork-board");
+const activeList = [cork_board, clipboard, clipboard_content];
+
+activeList.forEach(x=>x.classList.toggle("active"));
+
+
+function toggleActive() {
+    console.log("Clipboard has been pressed...");
+    activeList.forEach(x=>x.classList.toggle("active"));
 }
+
+
+// console.log(clipboard.classList.contains(class))
+
+clipboard.addEventListener("click", toggleActive)
+
+clipboard_content.addEventListener("click", e => {
+    clipboard.classList.contains('active')
+        ? e.stopPropagation()
+        : console.log("Propagation wasn't stopped...")
+})
+
+// clipboard_content.removeEventListener("click", e => {
+//     e.stopPropagation()
+// })
+
+// clipboard_content.removeEventListener("click", (e)=>{e.})
