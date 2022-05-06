664902b087d9dcbaea857f36cdefbc7bd8858a79
sergei_panin <s.panin1976@mail.ru>
2022-05-06 16:51:54 +0200

    add bookmarks

--- main.js
+++ main.js
@@ -1,51 +1,21 @@
-// const aside_toggle = document.querySelector(".aside-toggle");
-// const tile = document.querySelectorAll(".tile");
-
-// aside_toggle.addEventListener("click", function(){
-//     document.querySelector("aside").classList.toggle("active");
-//     document.getElementById("wrapper").classList.toggle("active");
-//     document.getElementById("aside-toggle-icon").classList.toggle("fa-arrow-right");
-//     aside_toggle.classList.toggle("active");
-//     // console.log("Lohi")
-// });
-
-// console.log(tile);
-// for (let i=0; i<tile.length; i++) {
-//     const tile_name = tile[i].querySelector(".tile-name");
-//     tile_name.addEventListener("click", () => {
-//         // tile[i].querySelector(".tile-content").classList.toggle("active");
-//         tile[i].classList.toggle("active");
-//     })
-//     // console.log(tile[i].querySelector(".tile-name"));
-// }
-
 const clipboard = document.querySelector(".clipboard");
 const clipboard_content = clipboard.querySelector(".clipboard__content");
 const clipboard_paper = clipboard.querySelectorAll(".paper");
 const cork_board = document.getElementById("cork-board");
-const activeList = [cork_board, clipboard, clipboard_content, ...clipboard_paper];
 
-activeList.forEach(x=>x.classList.toggle("active"));
+const clipboadrdActiveList = [cork_board, clipboard, clipboard_content, ...clipboard_paper];
+
+clipboadrdActiveList.forEach(x=>x.classList.toggle("active"));
 
 
 function toggleActive() {
     console.log("Clipboard has been pressed...");
-    activeList.forEach(x=>x.classList.toggle("active"));
+    clipboadrdActiveList.forEach(x=>x.classList.toggle("active"));
 }
 
-
-// console.log(clipboard.classList.contains(class))
 
 clipboard.addEventListener("click", toggleActive)
 
 clipboard_content.addEventListener("click", e => {
-    clipboard.classList.contains('active')
-        ? e.stopPropagation()
-        : console.log("Propagation wasn't stopped...")
+    if (clipboard.classList.contains('active')) e.stopPropagation()
 })
-
-// clipboard_content.removeEventListener("click", e => {
-//     e.stopPropagation()
-// })
-
-// clipboard_content.removeEventListener("click", (e)=>{e.})
