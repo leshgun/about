1e04e9fc6acf07845af54717a37c33ad924ef309
leshgun <nogalexey@gmail.com>
2022-05-13 06:58:58 +0200

    add new page

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const clipboard_content = clipboard.querySelector(".clipboard__content");
 const clipboard_paper = clipboard.querySelectorAll(".paper");
 const cork_board = document.getElementById("cork-board");
 
+const bookmarks = document.querySelectorAll(".bm");
+// const bm_info = bookmarks.querySelector(".info");
+// const bm_work = bookmarks.querySelector(".work");
+// const bm_educ = bookmarks.querySelector(".educ");
+
 const clipboadrdActiveList = [cork_board, clipboard, clipboard_content, ...clipboard_paper];
 
 clipboadrdActiveList.forEach(x=>x.classList.toggle("active"));
 
+function toggleActive2(event) {
+    console.log(event)
+    console.log(event.currentTarget.toogleList)
+    // event.currentTarget.toogleList.forEach(
+    //     x => event.currentTarget
+    // )
+}
+
+function switchClipboardPage() {
+    // for (let i = 0; i < bookmarks.length; i++) {
+        
+    // }
+    bookmarks.forEach(bm => {
+        bm.addEventListener("click", () => {
+            const bmType = bm.id.substring(3);
+            document.querySelectorAll(".page").forEach(x=>x.classList.remove("active"));
+            document.querySelector(".page-" + bmType).classList.add("active");
+        })
+    })
+}
 
 function toggleActive() {
     console.log("Clipboard has been pressed...");
@@ -14,8 +39,14 @@ function toggleActive() {
 }
 
 
-clipboard.addEventListener("click", toggleActive)
+clipboard.addEventListener("click", toggleActive);
 
 clipboard_content.addEventListener("click", e => {
     if (clipboard.classList.contains('active')) e.stopPropagation()
 })
+
+// bookmarks.addEventListener("click", toggleActive2);
+// bookmarks.toogleList = [".info", ".work", ".educ"]
+
+switchClipboardPage()
+
